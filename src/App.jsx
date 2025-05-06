70cf52f75c9768a28f194fa189315207e8098733
jaylenperez <jaylenestella24@gmail.com>
2025-05-06 12:55:35 -0500

    Task 1 Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-// src/App.jsx
 import React from 'react'
-import Greeting from './components/Greeting'
-import SocialLinks from './components/SocialLinks'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="app">
-      <Greeting />
-      {/* other content */}
-      <SocialLinks />
+      <Header />
     </div>
   )
 }
